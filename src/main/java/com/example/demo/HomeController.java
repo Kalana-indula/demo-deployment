73d0f5ff8a0116ed1dbd4be6ad8f7fa379539931
Kalana-indula <kalanasmedia@outlook.com>
2024-07-08 16:39:05 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping({"/", "/home"})
+    public String getStatus(){
+        return "Application is up and running";
+    }
+}
